9cfa0c86cf1b22feb652943890e10119eeedae39
purgalo <olasz.g48@gmail.com>
2026-02-07 17:38:25 +0100

    created update game endpoint

--- GameStore.Api/obj/Debug/net10.0/GameStore.Api.AssemblyInfo.cs
+++ GameStore.Api/obj/Debug/net10.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd4326ad587ac04ba9e7fa1d1cf7a76639dd9774")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0edf8787648ebec98b41ff959c513070ed9ecc03")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
