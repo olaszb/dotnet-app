bd4326ad587ac04ba9e7fa1d1cf7a76639dd9774
purgalo <olasz.g48@gmail.com>
2026-02-07 16:31:50 +0100

    implemented first get endpoint: /games

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,6 +1,18 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using GameStore.Api.Dtos;
 
-app.MapGet("/", () => "Hello World!");
+var builder = WebApplication.CreateBuilder(args);
+
+var app = builder.Build();
+app.UsePathBase("/api");
+
+List<GameDto> games = [
+    new (1, "Street Fighter II", "Fighting", 19.99M, new DateOnly(1992, 7, 15)),
+    new (2, "The Legend of Zelda: Ocarina of Time", "Action-Adventure", 29.99M, new DateOnly(1998, 11, 21)), 
+    new (3, "Super Mario 64", "Platformer", 24.99M, new DateOnly(1996, 6, 23)),
+    new (4, "Final Fantasy VII", "RPG", 39.99M, new DateOnly(1997, 1, 31)), 
+];
+
+// GET /games
+app.MapGet("/games", () => games);
 
 app.Run();
--- GameStore.Api/obj/Debug/net10.0/GameStore.Api.AssemblyInfo.cs
+++ GameStore.Api/obj/Debug/net10.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5f3ee46ef314ce98dd9a8900c9f1e782b24cbf4")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
